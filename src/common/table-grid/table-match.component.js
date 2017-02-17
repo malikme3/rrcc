24a7b982fc44ddfe708bdfa019b9b7e24d69b9c5
zulifqar ahmad <zulifqar.ahmad@hotmail.com>
2017-02-17 17:35:08 -0600

    Formatting code

--- src/common/table-grid/table-match.component.js
+++ src/common/table-grid/table-match.component.js
@@ -1,24 +1,19 @@
 (function () {
-"use strict";
-
-angular.module('common')
-.component('matchTable', {
-  templateUrl: 'src/common/table-grid/match-table.html',
-  bindings: {
-    data: '<'
-  }
-})
-.component('pointTable', {
-  templateUrl: 'src/common/table-grid/point-table.html',
-  bindings: {
-    data: '<'
-  }
-})
-.component('photoColumn', {
-  templateUrl: 'src/common/table-grid/photo-column.html',
-  bindings: {
-    data: '<'
-  }
-});
-
+    "use strict";
+    angular.module('common').component('matchTable', {
+        templateUrl: 'src/common/table-grid/match-table.html'
+        , bindings: {
+            data: '<'
+        }
+    }).component('pointTable', {
+        templateUrl: 'src/common/table-grid/point-table.html'
+        , bindings: {
+            data: '<'
+        }
+    }).component('photoColumn', {
+        templateUrl: 'src/common/table-grid/photo-column.html'
+        , bindings: {
+            data: '<'
+        }
+    });
 })();
