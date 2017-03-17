ae7f8afe50fcf6de19ebf302c38e6b01feedea43
malikme3 <zulifqar.ahmad@hotmail.com>
2017-03-16 21:29:47 -0500

    Update common.module.js

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -3,7 +3,7 @@
     angular.module('common', [])
         //.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
         .constant('ApiPath', 'https://shielded-meadow-69395.herokuapp.com').config(config)
-        .constant('ApiMVC', 'http://localhost:8001/ZulfiCricket').config(config);
+        .constant('ApiMVC', 'https://localhost:8443/ZulfiCricket').config(config);
     config.$inject = ['$httpProvider'];
 
     function config($httpProvider) {
