4dace77d28258ab892244255c74c21c2d0d72f61
zulifqar ahmad <zulifqar.ahmad@hotmail.com>
2017-03-24 23:13:17 -0500

    https req

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -3,8 +3,8 @@
     angular.module('common', [])
         //.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
         .constant('ApiPath', 'https://shielded-meadow-69395.herokuapp.com').config(config)
-        .constant('ApiMVC', 'https://localhost:8443/ZulfiCricket').config(config)
-        .constant('ApiMVC1', 'https://08ce2412.ngrok.io/ZulfiCricket').config(config);
+        .constant('ApiMVC1', 'https://localhost:8443/ZulfiCricket').config(config)
+        .constant('ApiMVC', 'https://1d5bc953.ngrok.io/ZulfiCricket').config(config);
     config.$inject = ['$httpProvider'];
 
     function config($httpProvider) {
